536de1bd0fc37658c3c350701218d2d566a9da49
XeryusTC <armadillo@onenetbeyond.org>
2015-11-05 15:10:21 +0100

    Inital FT

--- functional_tests/test_homepage.py
+++ functional_tests/test_homepage.py
@@ -3,11 +3,20 @@
 from .base import FunctionalTestCase
 
 class HomePageTest(FunctionalTestCase):
-    def test_temp_homepage_test(self):
-        """Test if Django is set up, can be removed later on."""
+    def test_landingpage_test(self):
+        """Test if we arrive on the landing page"""
         # Alice goes to the website
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
-        # The default first time setup page is loaded
-        body = self.browser.find_element_by_tag_name('body')
-        self.assertIn('It worked!', body.text)
+        # The title says the name of the site
+        self.assertIn('ProjMan', self.browser.title)
+
+        # There is a header with the site's name in it
+        header = self.browser.find_element_by_id('header')
+        self.assertIn('ProjMan', header.text)
+        # There is also a sign in button on the header
+        self.assertIn('Sign in', header.text)
+
+        # In the main body of the site there is also a sign in button
+        content = self.browser.find_element_by_id('content')
+        self.assertIn('Sign in', content.text)
