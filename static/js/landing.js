075a6b222b093eeb8c145b080f3eef41e8f94dd3
XeryusTC <armadillo@onenetbeyond.org>
2015-11-06 22:41:46 +0100

    Spiking indicated that it is easier to follow the separate login pages that Django uses
    - Remove everything in static since it is login popup related
    - Remove extra blocks from landing, but not from base.html because they may be
      useful in the future
    - The popup block is removed because it was single purpose
    - Buttons don't have the login class anymore because they were for popup
      related functionality

--- static/js/landing.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-	$('.login').click(function() {
-		popup = $('#login-popup').clone();
-		popup.attr('id', 'actual-login-popup');
-		popup.show();
-		mui.overlay('on', popup[0]);
-	});
-});
