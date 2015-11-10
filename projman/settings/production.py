c69e3d96c181c322e34f05494dd38d1b24d20095
XeryusTC <armadillo@onenetbeyond.org>
2015-11-10 22:21:39 +0100

    Build settings for production

--- projman/settings/production.py
+++ projman/settings/production.py
@@ -2,3 +2,29 @@
 from .base import *
 
 DEBUG = False
+DOMAIN = get_env_setting('PROJMAN_DOMAIN')
+ALLOWED_HOSTS = [
+    DOMAIN,
+    'www.' + DOMAIN,
+]
+
+SECRET_KEY = get_env_setting('PROJMAN_SECRET_KEY')
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR.child('templates'),],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.request',
+                'django.template.context_processors.i18n',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+                'landing.context_processors.site',
+            ],
+        },
+    },
+]
+
+SITE_ID = 1
