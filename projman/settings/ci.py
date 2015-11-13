73e0f3fd50e3ad37ca1d3558aa7e497c90258d47
XeryusTC <armadillo@onenetbeyond.org>
2015-11-13 15:36:16 +0100

    Initial Travic CI settup

--- projman/settings/ci.py
+++ projman/settings/ci.py
@@ -1,2 +1,17 @@
 # -*- coding: utf-8 -*-
 from .base import *
+
+DEBUG = True
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "projman_ci",
+        "USER": "postgres",
+        "PASSWORD": "",
+        "HOST": "localhost",
+        "PORT": "",
+    },
+}
+
+SECRET_KEY = "ContinuousIntegration"
