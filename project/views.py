35c071a989c8c546329605147e60b9cbf741a7da
XeryusTC <armadillo@onenetbeyond.org>
2015-11-14 23:26:03 +0100

    Pass most of the tests

--- project/views.py
+++ project/views.py
@@ -1,11 +1,23 @@
 # -*- coding: utf-8 -*-
 from django.views.generic import TemplateView, FormView
+from django.core.urlresolvers import reverse_lazy
 
 from project.forms import InlistForm
+from project.models import InlistItem
 
 class MainPageView(TemplateView):
     template_name = 'project/mainpage.html'
 
+
 class InlistView(FormView):
     template_name = 'project/inlist.html'
     form_class = InlistForm
+
+    def get_success_url(self):
+        return reverse_lazy('project:inlist')
+
+    def get_context_data(self, **kwargs):
+        context = super(InlistView, self).get_context_data(**kwargs)
+        context['inlist_items'] = InlistItem.objects.filter(
+            user=self.request.user)
+        return context
