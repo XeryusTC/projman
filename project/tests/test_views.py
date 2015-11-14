39ced4f10cd78f301c2bcfb795d89fc77624f106
XeryusTC <armadillo@onenetbeyond.org>
2015-11-14 23:24:27 +0100

    Refactor and add a test
    - Deduplicate some code in the tests
    - Remove url constants and reverse the url in a single place

--- project/tests/test_views.py
+++ project/tests/test_views.py
@@ -23,11 +23,13 @@ class TestMainPage(TestCase):
         self.assertTemplateUsed(response, 'project/base.html')
         self.assertTemplateUsed(response, 'project/mainpage.html')
 
+
 class InlistpageTest(TestCase):
     def setUp(self):
         self.alice = User.objects.create_user('alice', 'alice@test.com',
             'alice')
         self.bob = User.objects.create_user('bob', 'bob@test.com', 'bob')
+        self.client.login(username='alice', password='alice')
         self.url = reverse('project:inlist')
 
     def test_inlist_url_resolves_to_inlist_view(self):
@@ -36,31 +38,28 @@ class InlistpageTest(TestCase):
             views.InlistView.as_view().__name__)
 
     def test_inlist_uses_correct_templates(self):
-        response = self.client.get('/en/project/inlist')
+        response = self.client.get(self.url)
         self.assertTemplateUsed(response, 'html.html')
         self.assertTemplateUsed(response, 'project/base.html')
         self.assertTemplateUsed(response, 'project/inlist.html')
 
     def test_inlist_uses_inlist_form(self):
-        response = self.client.get(reverse('project:inlist'))
+        response = self.client.get(self.url)
         self.assertIsInstance(response.context['form'], InlistForm)
 
     def test_displays_only_items_for_that_user(self):
         item1 = factories.InlistItemFactory(text='item 1', user=self.alice)
         item2 = factories.InlistItemFactory(text='item 2', user=self.alice)
         item3 = factories.InlistItemFactory(text='item 3', user=self.bob)
-        self.client.login(username='alice', password='alice')
 
-        response = self.client.get(reverse('project:inlist'))
+        response = self.client.get(self.url)
 
         self.assertContains(response, 'item 1')
         self.assertContains(response, 'item 2')
         self.assertNotContains(response, 'item 3')
 
     def test_saves_POST_request_to_the_users_inlist(self):
-        self.client.login(username='alice', password='alice')
-
-        self.client.post(reverse('project:inlist'), data={'text': 'ecila'})
+        self.client.post(self.url, data={'text': 'ecila'})
 
         self.assertEqual(InlistItem.objects.count(), 1)
         item = InlistItem.objects.first()
@@ -68,15 +67,23 @@ class InlistpageTest(TestCase):
         self.assertEqual(item.user, alice)
 
     def test_POST_redirects_to_inlist_page(self):
-        self.client.login(username='alice', password='alice')
-        response = self.client.post(reverse('project:inlist'),
+        response = self.client.post('/en/project/inlist/',
             data={'text': 'test'})
-        self.assertRedirects(response, reverse('project:inlist'))
+        self.assertRedirects(response, '/en/project/inlist/')
 
     def test_invalid_input_saves_nothing_to_db(self):
-        self.client.login(username='alice', password='alice')
         response = self.client.post(self.url, data={'text': ''})
         self.assertEqual(InlistItem.objects.count(), 0)
+
+    def test_context_variables_contains_users_inlist_items(self):
+        item1 = factories.InlistItemFactory(text='item 1', user=self.alice)
+        item2 = factories.InlistItemFactory(text='item 2', user=self.alice)
+
+        response = self.client.get(self.url)
+
+        self.assertIn('inlist_items', response.context.keys())
+        self.assertIn(item1, response.context['inlist_items'])
+        self.assertIn(item2, response.context['inlist_items'])
 
     def test_form_invalid_input_shows_error_on_page(self):
         self.fail('Implement test')
