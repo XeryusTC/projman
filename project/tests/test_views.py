7c9dd52edf9220b50fc3cb2525fc8f75bb6d2cac
XeryusTC <armadillo@onenetbeyond.org>
2015-11-14 18:37:09 +0100

    Create view and form for the inlist page

--- project/tests/test_views.py
+++ project/tests/test_views.py
@@ -4,6 +4,7 @@ from django.test import TestCase
 import unittest
 
 from project import views
+from project.forms import InlistForm
 
 class TestMainPage(TestCase):
     def test_mainpage_url_resolves_to_mainpage_view(self):
@@ -16,3 +17,19 @@ class TestMainPage(TestCase):
         self.assertTemplateUsed(response, 'html.html')
         self.assertTemplateUsed(response, 'project/base.html')
         self.assertTemplateUsed(response, 'project/mainpage.html')
+
+class InlistpageTest(TestCase):
+    def test_inlist_url_resolves_to_inlist_view(self):
+        found = resolve(reverse('project:inlist'))
+        self.assertEqual(found.func.__name__,
+            views.InlistView.as_view().__name__)
+
+    def test_inlist_uses_correct_templates(self):
+        response = self.client.get('/en/project/inlist')
+        self.assertTemplateUsed(response, 'html.html')
+        self.assertTemplateUsed(response, 'project/base.html')
+        self.assertTemplateUsed(response, 'project/inlist.html')
+
+    def test_inlist_uses_inlist_form(self):
+        response = self.client.get(reverse('project:inlist'))
+        self.assertIsInstance(response.context['form'], InlistForm)
--- project/views.py
+++ project/views.py
@@ -1,5 +1,11 @@
 # -*- coding: utf-8 -*-
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, FormView
+
+from project.forms import InlistForm
 
 class MainPageView(TemplateView):
     template_name = 'project/mainpage.html'
+
+class InlistView(FormView):
+    template_name = 'project/inlist.html'
+    form_class = InlistForm
