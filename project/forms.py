d57acdcd7380a7f1043ce68336c3f61a96a09a12
XeryusTC <armadillo@onenetbeyond.org>
2015-11-15 14:03:55 +0100

    Update the text error to satisfy all tests

--- project/forms.py
+++ project/forms.py
@@ -8,7 +8,9 @@ EMPTY_TEXT_ERROR = _('You cannot add empty items')
 
 class InlistForm(forms.Form):
     text = forms.CharField(widget=forms.TextInput(
-        {'placeholder': _('What needs to be done?')}))
+        {'placeholder': _('What needs to be done?')}),
+        error_messages = {
+            'required': EMPTY_TEXT_ERROR})
 
     def save(self, user):
         return InlistItem(text=self.cleaned_data['text'],
