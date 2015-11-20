e79ecee966036e230d1c52039e6fefcf6e1435d0
XeryusTC <armadillo@onenetbeyond.org>
2015-11-20 21:07:29 +0100

    Style the inlist a bit better

--- functional_tests/pages/inlist.py
+++ functional_tests/pages/inlist.py
@@ -4,8 +4,8 @@ from page_objects import PageObject, PageElement, MultiPageElement
 class InlistPage(PageObject):
     add_box     = PageElement(name='text')
     add_button  = PageElement(xpath="//form//input[@id='submit-id-submit']")
-    thelist     = MultiPageElement(xpath="//table[@id='list']//tr/td[1]")
-    listrows    = MultiPageElement(xpath="//table[@id='list']//tr")
+    thelist     = MultiPageElement(css='#list .full-height')
+    listrows    = MultiPageElement(css='#list .mui-row')
     error_lists = MultiPageElement(css='.errorlist')
     delete_item = PageElement(link_text='DELETE', context=True)
 
