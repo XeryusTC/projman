b66196c500c143d0071c6052d95f522c4a5566d5
XeryusTC <armadillo@onenetbeyond.org>
2015-11-20 17:51:34 +0100

    Give InlistItem factory the ability to create text

--- projects/factories.py
+++ projects/factories.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
+import factory
 from factory.django import DjangoModelFactory
 
 class InlistItemFactory(DjangoModelFactory):
     class Meta:
         model = 'projects.InlistItem'
+
+    text = factory.Sequence(lambda n: 'Inlist item %d' %n)
