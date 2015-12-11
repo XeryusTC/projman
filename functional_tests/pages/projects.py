4bb69672c211e231f8fa3f93ef115c3a2168a851
XeryusTC <armadillo@onenetbeyond.org>
2015-12-11 18:32:13 +0100

    Make non field errors look the same as field errors

--- functional_tests/pages/projects.py
+++ functional_tests/pages/projects.py
@@ -26,7 +26,7 @@ class InlistPage(PageObject):
     add_button  = PageElement(xpath="//form//input[@id='submit-id-submit']")
     thelist     = MultiPageElement(css='#list .full-height')
     listrows    = MultiPageElement(css='#list .mui-row')
-    error_lists = MultiPageElement(css='.errorlist, #errorMsg')
+    error_lists = MultiPageElement(css='.errorlist')
     delete_item = PageElement(class_name='action-delete', context=True)
     convert_action = PageElement(class_name='action-convert', context=True)
 
@@ -39,7 +39,7 @@ class InlistDeletePage(PageObject):
 class ActionlistPage(PageObject):
     add_box     = PageElement(name='text')
     add_button  = PageElement(xpath="//form//input[@id='submit-id-submit']")
-    error_lists = MultiPageElement(css='.errorlist, #errorMsg')
+    error_lists = MultiPageElement(css='.errorlist')
     thelist     = MultiPageElement(css='#list .mui-row')
     checked_list = MultiPageElement(css='#checked .mui-row')
 
